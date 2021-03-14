0948bb6d17bb6c712ffbd1b6574c88d0e96e48fc
yonathansiregar <alfrenanday@gmail.com>
2021-03-14 08:49:43 +0700

    Soal "Easy Cipher" dan solusinya

--- Tugas 1 TIF A/Easy Cipher/Solution.java
+++ Tugas 1 TIF A/Easy Cipher/Solution.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner inp = new Scanner (System.in);
+        int pertambahan = inp.nextInt();
+        int a = inp.next().charAt(0);
+        int b = inp.next().charAt(0);
+        System.out.println((char) (a + pertambahan));
+        System.out.println((char) (b + pertambahan));
     }
 }
