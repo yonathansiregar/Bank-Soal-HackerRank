4d8480da5252348f3c04fb9c8018ccdfe9ecce3a
yonathansiregar <alfrenanday@gmail.com>
2021-03-14 08:44:36 +0700

    Soal "Kuis Bulanan" dan solusinya

---
+++ Tugas 1 TIF A/Kuis Bulanan/Solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        //selamat mengerjakan <3
+        Scanner sc = new Scanner (System.in);
+        int Jumlah_hari = sc.nextInt();
+        int hasil = Jumlah_hari / 30;
+        System.out.println (hasil + " kali kuis.");
+    }
+}
